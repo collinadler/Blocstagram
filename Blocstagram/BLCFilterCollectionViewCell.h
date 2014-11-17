2d0fad44332264664650b43b6bc90e8e440feb34
Collin Adler <collinadler@gmail.com>
2014-11-17 10:06:14 -0600

    added .gitignore

---
+++ Blocstagram/BLCFilterCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BLCFilterCollectionViewCell.h
+//  Blocstagram
+//
+//  Created by Collin Adler on 11/17/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCFilterCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Blocstagram/BLCFilterCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  BLCFilterCollectionViewCell.m
+//  Blocstagram
+//
+//  Created by Collin Adler on 11/17/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "BLCFilterCollectionViewCell.h"
+
+@implementation BLCFilterCollectionViewCell
+
+@end
